06c6f1c65509f88f3793cc201a49ab2b5e5b9d56
ddtommie <you@example.com>
2025-01-13 22:40:58 +0100

    why leave, explanation

--- js/detail.js
+++ js/detail.js
@@ -1,12 +1,9 @@
-// detail.js
 const topicHeaders = document.querySelectorAll('.topic-header');
 
 topicHeaders.forEach(header => {
   header.addEventListener('click', () => {
-    const content = header.nextElementSibling;
-    const isActive = header.parentElement.classList.contains('active'); // Check if it's active
 
-    // Collapse all OTHER items first if not active
+    let isActive = header.parentElement.classList.contains('active'); // Check if it's active
     if (!isActive) {
       topicHeaders.forEach(otherHeader => {
         if (otherHeader !== header) {
